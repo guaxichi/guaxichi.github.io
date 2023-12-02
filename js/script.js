d186d6b705db37b33d995a4611c8abcb5caa1b13
guaxichi <guxiangchun@hotmail.com>
2023-12-02 14:47:18 +0800

    Site updated: 2023-12-02 14:47:18

--- js/script.js
+++ js/script.js
@@ -40,21 +40,21 @@ document.ready(
         if (isDark) {
             document.getElementById("switch_default").checked = true;
             // mobile
-            document.getElementById("mobile-toggle-theme").innerText = "·  🌙"
+            document.getElementById("mobile-toggle-theme").innerText = "· 🌙"
         } else {
             document.getElementById("switch_default").checked = false;
             // mobile
-            document.getElementById("mobile-toggle-theme").innerText = "·  🌕"
+            document.getElementById("mobile-toggle-theme").innerText = "· 🌕"
         }
         _Blog.toggleTheme = function () {
             if (isDark) {
                 pagebody.classList.add('dark-theme');
                 // mobile
-                document.getElementById("mobile-toggle-theme").innerText = "·  🌙"
+                document.getElementById("mobile-toggle-theme").innerText = "· 🌙"
             } else {
                 pagebody.classList.remove('dark-theme');
                 // mobile
-                document.getElementById("mobile-toggle-theme").innerText = "·  🌕"
+                document.getElementById("mobile-toggle-theme").innerText = "· 🌕"
             }
             document.getElementsByClassName('toggleBtn')[0].addEventListener('click', () => {
                 if (pagebody.classList.contains('dark-theme')) {
